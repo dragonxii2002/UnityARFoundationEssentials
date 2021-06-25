2cefa460a2644a964aeb49ec32293ba3c548c4e2
Louis Cheung <67254284+dragonxii2002@users.noreply.github.com>
2021-06-25 09:20:19 +0800

    Adding 3D Shoes

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ARShoes()
+    {
+        SceneManager.LoadScene("HumanBodyTrackingShoes");
+    }
+    public void ARBodyDrawing()
+    {
+        SceneManager.LoadScene("HumanBodyTrackingDrawingWithOptions");
+    }
+    public void ARBodySuperPower()
+    {
+        SceneManager.LoadScene("HumanBodyTrackingWithSuperPowers");
+    }
+    public void HomePage()
+    {
+        SceneManager.LoadScene("HomePage");
+    }
+}
